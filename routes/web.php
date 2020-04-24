22ebcbdcece70ab91325ef242b9557937d22ae76
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-24 22:12:40 +0200

    Pass the test with created route and model

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/projects', function() {
+    // validate
+    // persist
+    App\Project::create(request(['title', 'description']));
+
+    // redirect
+});
