ecc54d5ba0c16eae96b480391e39089b5f648443
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-28 17:06:31 +0200

    Style project layout

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-    <a href="/projects">Go Back</a>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-gray-600 text-sm font-normal">My Projects</h2>
+            <a href="/projects/create" class="button">New project</a>
+        </div>
+    </header>
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-6">
+                    <h2 class="text-gray-600 font-normal text-lg mb-3">Tasks</h2>
+                    <div class="card">Lorem ipsum.</div>
+                    {{-- tasks --}}
+                </div>
+
+                <div>
+                    <h2 class="text-gray-600 font-normal text-lg mb-3">General notes</h2>
+
+                    {{-- general notes --}}
+                    <div class="card">Lorem ipsum.</div>
+                </div>
+
+            </div>
+            <div class="lg:w-1/4 px-3">
+                <div class="card">
+                    <h1>{{ $project->title }}</h1>
+                    <div>{{ $project->description }}</div>
+                    <a href="/projects">Go Back</a>
+                </div>
+            </div>
+        </div>
+    </main>
 @endsection
