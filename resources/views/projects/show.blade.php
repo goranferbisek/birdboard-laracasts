b9bc0b90f5853f1516eb75f463b254da4c1e6ad5
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-28 17:47:28 +0200

    Add dummy data and styling to a project

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,24 +3,33 @@
 @section('content')
     <header class="flex items-center mb-3 py-4">
         <div class="flex justify-between items-end w-full">
-            <h2 class="text-gray-600 text-sm font-normal">My Projects</h2>
+            <p class="text-gray-600 text-sm font-normal">
+                <a href="/projects" class="text-gray-600 text-sm font-normal no-underline" >
+                    My Projects
+                </a> / {{ $project->title }}
+            </p>
+
+
             <a href="/projects/create" class="button">New project</a>
         </div>
     </header>
     <main>
         <div class="lg:flex -mx-3">
             <div class="lg:w-3/4 px-3 mb-6">
-                <div class="mb-6">
+                <div class="mb-8">
                     <h2 class="text-gray-600 font-normal text-lg mb-3">Tasks</h2>
-                    <div class="card">Lorem ipsum.</div>
                     {{-- tasks --}}
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card">Lorem ipsum.</div>
                 </div>
 
                 <div>
                     <h2 class="text-gray-600 font-normal text-lg mb-3">General notes</h2>
 
                     {{-- general notes --}}
-                    <div class="card">Lorem ipsum.</div>
+                    <textarea class="card w-full" style="min-height: 200px;">Lorem ipsum.</textarea>
                 </div>
 
             </div>
