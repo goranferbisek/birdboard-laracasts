e947111bd0592ba14aa0e751dae7273d7fafe4f2
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-01 18:25:54 +0200

    Add a feature to update tasks as completed

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -20,7 +20,16 @@
                     <h2 class="text-gray-600 font-normal text-lg mb-3">Tasks</h2>
                     {{-- tasks --}}
                     @foreach ($project->tasks as $task)
-                        <div class="card mb-3">{{ $task->body }}</div>
+                        <div class="card mb-3">
+                            <form method="POST" action="{{ $project->path() .'/tasks/' . $task->id }}">
+                                @method('PATCH')
+                                @csrf
+                                <div class="flex">
+                                    <input name="body" type="text" value="{{ $task->body }}" class="w-full">
+                                    <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
+                                </div>
+                            </form>
+                        </div>
                     @endforeach
 
                     <form method="POST" action="{{ $project->path() . '/tasks' }}">
