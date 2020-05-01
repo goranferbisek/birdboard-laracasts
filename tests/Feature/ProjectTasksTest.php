34794ff882fcec4322c0a45aadf32f1eda6dfc77
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-05-01 20:35:43 +0200

    Test updating a task only by owner

--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -32,6 +32,20 @@ class ProjectTasksTest extends TestCase
     }
 
     /** @test */
+    public function only_the_owner_of_a_project_may_update_a_task()
+    {
+        $this->signIn();
+
+        $project = factory('App\Project')->create();
+        $task = $project->addTask('Test task');
+
+        $this->patch($project->path() . '/tasks/' .$task->id, ['body' => 'changed'])
+            ->assertStatus(403);
+
+        $this->assertDatabaseMissing('tasks', ['body' => 'changed']);
+    }
+
+    /** @test */
     public function a_project_can_have_tasks()
     {
         $this->signIn();
