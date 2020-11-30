83f2312cc7499764c4e975bbdb64732a290d7e27
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-11-30 21:03:21 +0100

    Add a new activity test

---
+++ tests/Feature/ActivityFeedTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Facades\Tests\Setup\ProjectFactory;
+use Tests\TestCase;
+
+class ActivityFeedTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function creating_a_project_generates_activity()
+    {
+        //$this->withExceptionHandling();
+        $project = ProjectFactory::create();
+
+        $this->assertCount(1, $project->activity);
+    }
+}
