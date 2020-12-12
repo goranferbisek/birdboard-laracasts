6a976c2a6c82779c947bd6a18adef9235a8173fe
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-12-12 12:49:29 +0100

    Refactor: make project events consistent with other events

--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -19,7 +19,7 @@ class TrigerActivityTest extends TestCase
         $this->assertCount(1, $project->activity);
 
         tap($project->activity->last(), function ($activity) {
-            $this->assertEquals('created', $activity->description);
+            $this->assertEquals('created_project', $activity->description);
             $this->assertNull($activity->changes);
         });
     }
@@ -34,7 +34,7 @@ class TrigerActivityTest extends TestCase
         $this->assertCount(2, $project->activity);
 
         tap($project->activity->last(), function ($activity) use ($originalTitle) {
-            $this->assertEquals('updated', $activity->description);
+            $this->assertEquals('updated_project', $activity->description);
 
             $expected = [
                 'before' => ['title' => $originalTitle],
