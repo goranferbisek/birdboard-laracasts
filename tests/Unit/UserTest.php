4b9840037c7ba52ba4a87338c31b8b03fbccba05
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-12-16 18:54:49 +0100

    Get users accessible projects with a unit test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -16,5 +18,19 @@ class UserTest extends TestCase
         $user = factory('App\User')->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    /** @test */
+    public function a_user_has_accessible_projects()
+    {
+        $john = $this->signIn();
+        $project = ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $sally = factory(User::class)->create();
+        ProjectFactory::ownedBy($sally)->create()->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
     }
 }
