fe793e85e2bce0c6765946067d89fe224f9c96cb
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-12-16 18:52:54 +0100

    Test if user sees projects where he was invited

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -51,6 +51,16 @@ class ManageProjectsTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_see_all_projects_they_have_been_invited_to_on_their_dashboard()
+    {
+        $user = $this->signIn();
+        $project = ProjectFactory::create();
+        $project->invite($user);
+
+        $this->get('/projects')->assertSee($project->title);
+    }
+
+    /** @test */
     public function unauthorized_users_cannot_delete_a_projects()
     {
         $project = ProjectFactory::create();
