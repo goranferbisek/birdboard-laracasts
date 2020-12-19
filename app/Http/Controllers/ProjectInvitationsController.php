9811391d5253f462ddb1843f764d129d1985382b
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-12-19 11:19:40 +0100

    Invite users to project with tests

---
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use App\User;
+use Illuminate\Http\Request;
+
+class ProjectInvitationsController extends Controller
+{
+    /**
+     * user invites a member based on an email address
+     * you can invite only user that are members on birboard
+     *
+     * otherwise we would have to impletent sending invition links to a user
+     * email address and offer the registration to birboard
+     * and finally invite them as project members
+     */
+    public function store(Project $project)
+    {
+        $user = User::whereEmail(request('email'))->first();
+        $project->invite($user);
+    }
+}
