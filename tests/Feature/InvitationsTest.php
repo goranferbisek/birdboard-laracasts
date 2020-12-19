2d0bfab874e9d11d0d698df283fe0611eda6f476
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-12-19 11:19:40 +0100

    Invite users to project with tests

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -14,12 +14,26 @@ class InvitationsTest extends TestCase
     /** @test */
     public function a_project_can_invite_a_user()
     {
+        $this->withoutExceptionHandling();
+        $project = ProjectFactory::create();
+        $userToInvite = factory(User::class)->create();
+
+        $this->actingAs($project->owner)->post($project->path() . '/invitations', [
+            'email' => $userToInvite->email
+        ]);
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    public function invited_users_may_update_project_details()
+    {
         $project = ProjectFactory::create();
         $project->invite($newUser = factory(User::class)->create());
 
         $this->signIn($newUser);
-        $this->post
-            (action('ProjectTasksController@store', $project),
+        $this->post(
+            action('ProjectTasksController@store', $project),
             $task = ['body' => 'foo task']
         );
 
