4c81ce745652e689cbe0eff222c81eb892e522d7
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-12-20 15:17:07 +0100

    Hide invitation form from unauthorized users

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -75,21 +75,9 @@
             <div class="lg:w-1/4 px-3">
                 @include('projects.card')
                 @include('projects.activity.card')
-
-                <div class="card flex flex-col mt-3">
-                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-primary-light pl-4">
-                        Invite a user
-                    </h3>
-                    <form method="POST" action="{{ $project->path() . '/invitations' }}">
-                        @csrf
-
-                        <div class="mb-3">
-                            <input type="email" name="email" class="border border-gray-600 rounded w-full py-2 px-3" placeholder="Email address">
-                        </div>
-                        <button type="submit" class="button">Invite</button>
-                    </form>
-                    @include('errors', ['bag' => 'invitations'])
-                </div>
+                @can('manage', $project)
+                    @include('projects.invite')
+                @endcan
             </div>
         </div>
     </main>
