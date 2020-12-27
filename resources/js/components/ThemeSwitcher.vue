e32d950c7ad7bce227fdd8c7dd9559bcda52d2eb
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-12-27 17:06:37 +0100

    Make a theme list dynamic

--- resources/js/components/ThemeSwitcher.vue
+++ resources/js/components/ThemeSwitcher.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="mr-8 flex items-center">
-        <button class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none"
-            :class="{ 'border-accent': selectedTheme == 'theme-light' }"
-            style="background: #f5f6f9"
-            @click="selectedTheme = 'theme-light'"
-        ></button>
-
-        <button class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none"
-            :class="{ 'border-accent': selectedTheme == 'theme-dark' }"
-            style="background: #222"
-            @click="selectedTheme = 'theme-dark'"
+        <button v-for="(color, theme) in themes"
+            class="rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none"
+            :class="{ 'border-accent': selectedTheme == theme }"
+            :style="{ backgroundColor: color}"
+            @click="selectedTheme = theme"
+            :key="theme"
         ></button>
     </div>
 </template>
@@ -18,13 +14,23 @@
     export default {
         data() {
             return {
+                themes: {
+                    'theme-light': '#f5f6f9',
+                    'theme-dark': '#222'
+                },
                 selectedTheme: 'theme-light'
             }
+        },
+
+        created() {
+            this.selectedTheme = localStorage.getItem('theme') || 'theme-light';
         },
 
         watch: {
             selectedTheme() {
                 document.body.className = document.body.className.replace(/theme-\w+/, this.selectedTheme);
+
+                localStorage.setItem('theme', this.selectedTheme);
             }
         }
     }
